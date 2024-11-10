4bdfea7bf9646291b65c292627ebf6ad860042f9
Owen Bélanger-Love <belangerloveo@gmail.com>
2024-11-10 14:58:15 -0500

    Fix error in BronzeCount and add documentation

--- Jr/2024/BronzeCount.py
+++ Jr/2024/BronzeCount.py
@@ -1,19 +1,22 @@
+# Get the amount of participants
 count = int(input())
 
+# Get the scores
 values: list[int] = []
 for i in range(count):
     v = int(input())
     values.append(v)
 
-
+# Sort the scores and revers (largest to smallest)
 values.sort()
 values.reverse()
 
+# Remove all first place wins and second places wins
+for i in range(2):
+    values = values[values.count(values[0]):]
 
+# Get third place value
 v = values[0]
-values = values[values.count(v):]
 
-v = values[0]
-
-
+# Get and print results
 print(f"{v} {values.count(v)}")
